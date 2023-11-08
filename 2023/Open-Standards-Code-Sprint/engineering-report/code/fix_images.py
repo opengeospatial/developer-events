d592e509899ebbd7d7dae5e8e4e04f40d808edc2
Gobe Hobona <ghobona@opengeospatial.org>
2023-11-08 12:08:27 +0000

    removed code for fixing broken images

--- 2023/Open-Standards-Code-Sprint/engineering-report/code/fix_images.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import os
-
-for f in os.listdir('/Users/gobehobona/Documents/GitHub/developer-events/2023/Open-Standards-Code-Sprint/engineering-report/images2'):
-    if (f.endswith('.png')):
-        print(f)
-        imageIn = Image.open('/Users/gobehobona/Documents/GitHub/developer-events/2023/Open-Standards-Code-Sprint/engineering-report/images2/'+f)
-        imageIn.save('/Users/gobehobona/Documents/GitHub/developer-events/2023/Open-Standards-Code-Sprint/engineering-report/images/'+f,format="png")
